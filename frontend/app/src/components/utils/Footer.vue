aefe53df10177173280f582e02890abbc736db50
KH1118 <s1230119@gmail.com>
2025-06-21 10:51:52 +0900

    トップページの雛形作成

---
+++ frontend/app/src/components/utils/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="bg-white border-t mt-10">
+    <div class="container mx-auto px-4 py-6 text-center text-gray-500">
+      &copy; 2025 Yosakoi Portal
+    </div>
+  </footer>
+</template>
