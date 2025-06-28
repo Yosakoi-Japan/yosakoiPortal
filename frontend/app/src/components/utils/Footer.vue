356678fc8b86036aab5cf7d9015d3b7ad88caac2
Collie <collie@CollienoMacBook-Pro.local>
2025-06-29 00:00:50 +0900

    feat:利用規約ページの追加

--- frontend/app/src/components/utils/Footer.vue
+++ frontend/app/src/components/utils/Footer.vue
@@ -1,7 +1,17 @@
 <template>
-  <footer class="bg-white border-t mt-10">
-    <div class="container mx-auto px-4 py-6 text-center text-gray-500">
-      &copy; 2025 Yosakoi Portal
+  <footer class="bg-white border-t">
+    <div
+      class="container mx-auto px-4 py-6 flex justify-between items-center flex-wrap"
+    >
+      <div class="flex flex-wrap gap-6 text-sm">
+        <NuxtLink to="/terms" class="hover:text-blue-600">利用規約</NuxtLink>
+      </div>
+    </div>
+
+    <div class="border-t border-gray-700">
+      <div class="container mx-auto px-4 py-6 text-center text-gray-500">
+        &copy; 2025 Yosakoi Portal
+      </div>
     </div>
   </footer>
 </template>
