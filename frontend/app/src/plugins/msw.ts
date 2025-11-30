c5a523b7242977d6a3fcc90ad2865450912adecf
collie <s1250172a@gmail.com>
2025-11-30 11:38:38 +0900

    SEO対策でSSRを有効化

--- frontend/app/src/plugins/msw.ts
+++ frontend/app/src/plugins/msw.ts
@@ -1,6 +1,10 @@
-import { worker } from "~/mocks/browser";
-
 export default defineNuxtPlugin(async () => {
+  if (import.meta.server) {
+    return;
+  }
+
+  const { worker } = await import("~/mocks/browser");
+
   // TODO:local環境でのみMSWを有効にする
   await worker.start({
     onUnhandledRequest: "bypass",
